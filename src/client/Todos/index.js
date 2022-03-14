5f567a235bb9d3654e4034911011366a572b2744
Leigh Garland <leigh.garland@johnlewis.co.uk>
2022-03-14 14:32:31 +0000

    Added Todos components

---
+++ src/client/Todos/index.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Todos = () => {
+	return (
+		<>
+			<h2>Shit you have to do</h2>
+		<ul>
+		</ul>
+			<p>If you can see this... something's working</p>
+		</>
+
+	)
+}
+
+export default Todos;
